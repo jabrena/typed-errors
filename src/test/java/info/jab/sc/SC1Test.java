861f48543a9944221d3f2d4d828bbebc9b373323
Juan Antonio Breña Moral <bren@juanantonio.info>
2024-06-21 21:27:29 +0200

    Initial work with Structural concurrency (#64)

---
+++ src/test/java/info/jab/sc/SC1Test.java
@@ -0,0 +1,151 @@
+package info.jab.sc;
+
+import info.jab.fp.util.Either;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.StructuredTaskScope;
+import java.util.concurrent.StructuredTaskScope.Subtask;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Function;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+public class SC1Test {
+
+    @Test
+    void should_work() {
+        try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
+            Subtask<UserInfo> userInfoTask = scope.fork(() -> getUserInfo(1));
+
+            scope.join().throwIfFailed();
+
+            System.out.println(userInfoTask.state());
+            final var userInfo = userInfoTask.get();
+            System.out.println("User: " + userInfo);
+        } catch (ExecutionException | InterruptedException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Test
+    void should_not_work() {
+        try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
+            Subtask<UserInfo> userInfoTask = scope.fork(() -> getUserInfo(2));
+
+            scope.join().throwIfFailed();
+
+            System.out.println(userInfoTask.state());
+            final var userInfo = userInfoTask.get();
+            System.out.println("User: " + userInfo);
+        } catch (ExecutionException | InterruptedException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Test
+    void should_work_with_either() {
+        try (var scope = new StructuredTaskScope()) {
+            Subtask<Either<SubsystemProblems, UserInfo>> userInfoTask = scope.fork(() -> getUserInfo2(1));
+
+            scope.join();
+
+            System.out.println(userInfoTask.state());
+            final var userInfo = userInfoTask.get().get();
+            System.out.println("User: " + userInfo);
+        } catch (InterruptedException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Test
+    void should_work_with_either_error() {
+        try (var scope = new StructuredTaskScope()) {
+            Subtask<Either<SubsystemProblems, UserInfo>> userInfoTask = scope.fork(() -> getUserInfo2(2));
+
+            scope.join();
+
+            System.out.println(userInfoTask.state());
+            if (userInfoTask.get().isLeft()) {
+                var result = userInfoTask.get().fold(ex -> ex.toString(), ok -> ok);
+                System.out.println(result);
+            }
+        } catch (InterruptedException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Test
+    void should_work_with_either_timeout() {
+        try (var scope = new StructuredTaskScope()) {
+            Subtask<Either<SubsystemProblems, UserInfo>> userInfoTask = scope.fork(() -> getUserInfo3(3));
+
+            //Subtask<List<Follower>> mostFollowersTask = scope.fork(() -> getFollowers(userInfoTask.get()));
+
+            try {
+                scope.joinUntil(Instant.now().plus(Duration.ofMillis(1000)));
+            } catch (TimeoutException e) {
+                //scope.shutdown();
+            }
+
+            System.out.println(userInfoTask.state());
+            /*
+            if (userInfoTask.get().isLeft()) {
+                var result = userInfoTask.get().fold(ex -> ex.toString(), ok -> ok);
+                System.out.println(result);
+            }
+            */
+        } catch (InterruptedException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private UserInfo getUserInfo(Integer userId) {
+        if (userId > 1) {
+            throw new RuntimeException("Katakroker");
+        }
+        return new UserInfo(userId, "UserName");
+    }
+
+    enum SubsystemProblems {
+        DATABASE_ERROR,
+        COMPUTATION_ERROR,
+    }
+
+    private Either<SubsystemProblems, UserInfo> getUserInfo2(Integer userId) {
+        if (userId > 1) {
+            return Either.left(SubsystemProblems.DATABASE_ERROR);
+        }
+        UserInfo result = new UserInfo(userId, "UserName");
+        return Either.right(result);
+    }
+
+    private Either<SubsystemProblems, UserInfo> getUserInfo3(Integer userId) {
+        if (userId == 2) {
+            return Either.left(SubsystemProblems.DATABASE_ERROR);
+        } else if (userId > 2) {
+            System.out.println("Computation with latency");
+            delay(2000);
+            return Either.left(SubsystemProblems.COMPUTATION_ERROR);
+        }
+        UserInfo result = new UserInfo(userId, "UserName");
+        return Either.right(result);
+    }
+
+    private void delay(int param) {
+        try {
+            Thread.sleep(param);
+        } catch (InterruptedException ex) {
+            // Empty on purpose
+        }
+    }
+
+    record UserInfo(Integer userId, String username) {}
+
+    private List<Follower> getFollowers(UserInfo userInfo) {
+        return List.of(new Follower("follower1"), new Follower("follower2"));
+    }
+
+    record Follower(String nombre) {}
+}
